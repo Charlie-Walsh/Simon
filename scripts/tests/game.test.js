e966475a99ae9ce3813367a369a67f59fc785dee
Charlie-Walsh <charlie.walsh.141@gmail.com>
2022-06-07 20:58:53 +0000

    testinand building newGame function and showScore function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,27 +2,55 @@
  * @jest-environment jsdom
  */
 
-const game = require("../game")
+ const { hasUncaughtExceptionCaptureCallback } = require("process");
+const { game, newGame, showScore } = require("../game");
 
-beforeAll (() => {
-    let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
-})
 
-describe("game object contains correct keys", () => {
-    test("score key exists", () => {
-        expect("score" in game).toBe(true);
+ beforeAll(() => {
+     let fs = require("fs");
+     let fileContents = fs.readFileSync("index.html", "utf-8");
+     document.open();
+     document.write(fileContents);
+     document.close();
+ });
+ 
+ describe("game object contains correct keys", () => {
+     test("score key exists", () => {
+         expect("score" in game).toBe(true);
+     });
+     test("currentGame key exists", () => {
+         expect("currentGame" in game).toBe(true);
+     });
+     test("playerMoves key exists", () => {
+         expect("playerMoves" in game).toBe(true);
+     });
+     test("choices key exists", () => {
+         expect("choices" in game).toBe(true);
+     });
+     test("choices contain correct ids", () => {
+         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+     });
+ });
+
+ describe("New Game works correctly", () => {
+     beforeAll(() => {
+         game.score = 42;
+         game.currentGame = ["button1", "button2"];
+         game.playerMoves = ["button1", "button2"];
+         document.getElementById("score").innerText = "42";
+
+         newGame();
+     })
+    test("should set game score to 0", () => {
+        expect(game.score).toEqual(0);
     });
-    test("currentGame key exists", () => {
-        expect("currentGame" in game).toBe(true);
+    test("should clear playerMoves array", () => {
+        expect(game.playerMoves.length).toEqual(0);
+    })
+    test("should clear currentGames array", () => {
+        expect(game.currentGame.length).toEqual(0);
+    })
+    test("should display 0 for the element with the id of score", () => {
+        expect(document.getElementById("score").innerText).toBe(0);
     });
-    test("playerMoves key exists", () => {
-        expect("playerMoves" in game).toBe(true);
-    });
-    test("choices key exists", () => {
-        expect("choices" in game).toBe(true);
-    });
-});
+ });
